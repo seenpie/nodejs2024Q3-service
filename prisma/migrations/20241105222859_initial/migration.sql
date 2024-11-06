b4d6455d85514cbe6b544f3f59d0b4f85d7bfab2
seenpie <bananananasez@gmail.com>
2024-11-06 15:44:55 +0300

    feat: implement Artist model for prisma

--- prisma/migrations/20241105222859_initial/migration.sql
+++ prisma/migrations/20241105222859_initial/migration.sql
@@ -7,3 +7,10 @@ CREATE TABLE "User" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
+-- CreateTable
+CREATE TABLE "Artist" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "grammy" BOOLEAN NOT NULL
+);
