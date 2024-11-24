9959e93d8f45ac731378a4a198a5661bd4f140da
seenpie <bananananasez@gmail.com>
2024-11-24 22:53:59 +0300

    feat: implement custom exception-filter for app

---
+++ src/exception-filter/exception-filter.service.ts
@@ -0,0 +1,49 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
+import { LoggingService } from "@/logging/logging.service";
+
+@Catch()
+export class ExceptionFilterService implements ExceptionFilter {
+  constructor(private readonly loggingService: LoggingService) {
+    process.on("uncaughtException", (error) => {
+      this.loggingService.error(
+        `Uncaught Exception: ${error.message}`,
+        error.stack,
+      );
+      process.exit(1);
+    });
+
+    process.on("unhandledRejection", (reason) => {
+      this.loggingService.error(
+        `Unhandled Rejection: ${
+          reason instanceof Error ? reason.message : reason
+        }`,
+        reason instanceof Error ? reason.stack : "",
+      );
+    });
+  }
+
+  catch(exception: HttpException | any, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse();
+
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    const message =
+      exception instanceof HttpException
+        ? exception.getResponse()
+        : { statusCode: status, message: "Internal server error" };
+
+    this.loggingService.logResponse(message, status);
+
+    response.status(status).json(message);
+  }
+}
